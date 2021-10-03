702e2ad092549f9cec4c6082c54463e239115ffe
Derrick Alden <jebus_for_life@yahoo.com>
2021-10-02 19:01:26 -0700

    functionality added

--- Game.gd
+++ Game.gd
@@ -20,24 +20,34 @@ var rng = RandomNumberGenerator.new()
 var fate
 var fateList
 var fateDie
+var genCount
+var isPaused
 # 1 = life   |   4 = dead
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	genCount = 0
 	xRange = 100
 	yRange = 100
 	setupCount = 3
-	running = true
+	isPaused =false
 	rng.randomize()
 	fateList = PoolVector2Array()
 	fateDie = PoolVector2Array()
 	#clearBoard()
 	#setupBoard()
-	
+	running = true
 	while(running):
-		processGeneration()
-		processBoard()
-		yield(get_tree().create_timer(.5), "timeout")
+		if(!isPaused):
+			processGeneration()
+			processBoard()
+			updateUI()
+			yield(get_tree().create_timer(.5), "timeout")
+		else:
+			yield(get_tree().create_timer(.5), "timeout")
 	pass # Replace with function body.
+
+func updateUI():
+	get_node("GridContainer/GenCount").text = String(genCount)
 
 func processBoard():
 	for entry in fateList:
@@ -46,32 +56,33 @@ func processBoard():
 	for entry in fateDie:
 		#print(entry)
 		get_node("TileMap").set_cellv(entry,4)
-	fateList.resize(0)
-	fateDie.resize(0)
+	fateList = []
+	fateDie = []
+	genCount = genCount + 1
 	pass
-
-func setupBoard():
+var firstGenPulsar = [Vector2(13, 24), Vector2(14, 23), Vector2(14, 24), Vector2(14, 25), Vector2(15, 23), Vector2(15, 25), Vector2(16, 23), Vector2(16, 24), Vector2(16, 25), Vector2(17, 24)];
+func presetBoard1():
+	isPaused = true
 	setupIndex = 0
-	clearBoard()
-	while(setupIndex < setupCount):
-		for x in range(xRange):
-			for y in range(yRange):
-				currentVector = Vector2(x,y)
-				var my_random_number = rng.randi_range(1, 10)
-				if(my_random_number == 1):
-					get_node("TileMap").set_cellv(currentVector,1)
-					setupIndex = setupIndex + 1
-					break
-				else:
-					get_node("TileMap").set_cellv(currentVector,4)
+	fateList = firstGenPulsar
+	processBoard()
+	yield(get_tree().create_timer(.5), "timeout")
+	isPaused = false
 	pass
 
 func clearBoard():
 	print("Clearing Board")
+	isPaused = true
+	genCount = 0
+	updateUI()
+	fateList = []
+	fateDie = []
 	for x in range(xRange):
 		for y in range(yRange):
 			currentVector = Vector2(x,y)
-			get_node("TileMap").set_cellv(currentVector,4)
+			#get_node("TileMap").set_cellv(currentVector,4)
+			fateDie.append(currentVector)
+	processBoard()
 
 func processGeneration():
 	for x in range(xRange):
@@ -100,10 +111,10 @@ func processGeneration():
 			if(myIndex == 1):
 				if(neighborsAlive < 2 || neighborsAlive > 3):
 					fate = false
-					fateList.append(currentVector)
+					fateDie.append(currentVector)
 				else:
 					fate = true
-					fateDie.append(currentVector)
+					fateList.append(currentVector)
 			else:
 				if(neighborsAlive == 3):
 					fate = true
@@ -114,10 +125,12 @@ func processGeneration():
 	pass
 
 func _on_Stop_pressed():
+	isPaused = true
 	pass # Replace with function body.
 
 
 func _on_Start_pressed():
+	isPaused = false
 	pass # Replace with function body.
 
 
@@ -125,7 +138,6 @@ func _on_Clear_pressed():
 	clearBoard()
 	pass # Replace with function body.
 
-
-func _on_Reset_pressed():
-	setupBoard()
+func _on_Preset1_pressed():
+	presetBoard1()
 	pass # Replace with function body.
